a99612be6d42ee106ac9674ff89a2341c056c803
IgorDemidov <igr.demidov@gmail.com>
2015-07-10 11:41:10 +0300

    Nuton

---
+++ NutonRoot.Task1.Library/NutonCalc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NutonRoot.Task1.Library
+{
+    public class NutonCalc
+    {
+        /// <summary>
+        ///  This method calculates the value of the n-th root of the  argument arg to within e by Newton method.
+        /// </summary>
+        public double RootOf(double arg, double n, double delta)
+        {
+            if ((arg<0) && (n%2!=0))
+            {
+                return Double.NaN;
+            }
+            else
+            {
+                double x = arg;
+                double next = x;
+                do
+                {
+                    x = next;
+                    next = (((n - 1) * x) + (arg / Math.Pow(x,(n-1)))) / n;
+                } while (Math.Abs(x - next) > delta);
+
+                return x;
+            }
+            
+        }
+    }
+}
---
+++ NutonRoot.Task1.Test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NutonRoot.Task1.Library;
+
+
+namespace NutonRoot.Task1.Test
+{
+    
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            NutonCalc calc = new NutonCalc();
+            Console.WriteLine(calc.RootOf(2, 2, 0.00001));
+            Console.WriteLine(Math.Pow(2,0.5));
+            Console.ReadLine();
+        }
+    }
+}
