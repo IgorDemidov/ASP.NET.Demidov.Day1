087d83bfb999a7775af4b2f1d3f423f418b8291f
IgorDemidov <igr.demidov@gmail.com>
2015-07-11 21:16:35 +0300

    вчерашнее

---
+++ JaggedArray.Task2/ExplorerMin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JaggedArray.Task2
+{
+    class ExplorerMin: IRowExplorer
+    {
+        public int Explore(int[] row)
+        {
+            int targetMin = row[1];
+            for (int i = 1; i < row.Length; i++)
+            {
+                if (row[i - 1] > row[i])
+                {
+                    targetMin = row[i];
+                }
+            }
+            return targetMin;
+        }
+    }
+}
